27b3b105af0923bdc12f8b28d1860ad8a540b150
Jim Hague <jim@sinodun.com>
2019-03-13 12:35:47 +0000

    Make changes requested by mkevenaar.
    This package has older packages with an newer version. NOTE: People who have this package already installed, will not update this version.
    
    Before we can approve this package, could you please make the following changes:
    
        Since you are embedding the software, the checksum and checksumtype in your chocolateyInstall.ps1 file should be removed.
    
        Install-ChocolateyPackage is for downloading and installing software, please switch to Install-ChocolateyInstallPackage
    
        The wiki link you have provided in your VERIFICATION.txt file is non version specific. Therefore if the developer pushes a new version of the software, the checksums are changed. Could you please add them to your VERIFICATION.txt? This is not required, but will make sure that in the future verifications can still be made.
    
        Could you please update the <owners> field in the nuspec file to match your Chocolatey username / your full name?
    
        Could you please update your <licenseUrl> to point to the github repo and not via jsdeliver?
    
        Your <packageSourceUrl> is invalid, could you please update it?

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -10,16 +10,11 @@ $packageArgs = @{
 
   softwareName  = 'Stubby'
 
-  checksum      = '%CHECKSUM%'
-  checksumType  = 'sha256'
-  checksum64    = '%CHECKSUM64%'
-  checksumType64= 'sha256'
-
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }
 
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
 
 $packageName = $packageArgs.packageName
 $softwareName = $packageArgs.softwareName
